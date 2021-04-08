0fbbf15463928eec579321ab10075388d859b372
waffillka <waffilka@gmail.com>
2021-04-08 22:49:13 +0300

    Add project files.

---
+++ UDPClient/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDPClient
+{
+
+    class Program
+    {
+        public static void Main()
+        {
+            byte[] data = new byte[256];
+            string temp;
+
+            string address = "127.0.0.1";
+
+            UdpClient server = new UdpClient(address, 9050);
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+
+            Console.WriteLine("{0}", sender.ToString());
+
+            while (true)
+            {
+                data = new byte[256];
+                Console.Write("Введите сообщение: ");
+                temp = Console.ReadLine();
+                Console.WriteLine($"ОТПРАВЛЕНО {DateTime.Now.ToString()}:\n {temp}");
+                data = Encoding.UTF8.GetBytes(temp);
+
+                server.Send(data, data.Length);
+
+                if (temp == "exit") break;
+
+                data = server.Receive(ref sender);
+                Console.WriteLine($"ПРИНЯТО {sender.ToString() + "||" + DateTime.Now.ToString()}:");
+                temp = Encoding.UTF8.GetString(data, 0, data.Length);
+                Console.WriteLine(temp + "\n");
+            }
+            Console.WriteLine("Остановка клиента...");
+            server.Close();
+        }
+
+    }
+}
---
+++ UDPServer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPServer
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int bytes;
+            byte[] data = new byte[256];
+            IPEndPoint ipLocal = new IPEndPoint(IPAddress.Any, 9050);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            int m;
+            try
+            {
+                socket.Bind(ipLocal);
+
+                IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+                EndPoint remoteIP = (EndPoint)(sender);
+                while (true)
+                {
+                    data = new byte[256];
+                    bytes = socket.ReceiveFrom(data, ref remoteIP);
+
+                    string temp = Encoding.UTF8.GetString(data, 0, bytes);
+                    if (temp == "exit") break;
+                    Console.WriteLine($"ПРИНЯТО ({remoteIP.ToString() + "||" + DateTime.Now.ToString()}):\n {temp}");
+
+                    if (temp.Length % 5 == 0)
+                        temp = temp.Count(x => x == '{' || x == '}' || x == '[' || x == ']' || x == '(' || x == ')').ToString();
+                    else
+                        temp = $"Длина({temp.Length}) строка \"{temp}\" НЕ кратно 5!";
+
+                    Console.WriteLine($"ОТПРАВЛЕНО ({DateTime.Now.ToString()}):\n {temp}");
+                    data = Encoding.UTF8.GetBytes(temp);
+                    socket.SendTo(data, data.Length, SocketFlags.None, remoteIP);
+                }
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error);
+            }
+        }
+    }
+}
